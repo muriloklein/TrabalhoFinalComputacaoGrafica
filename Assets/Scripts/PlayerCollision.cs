2bdc4dbae6cd875d24b977b7c2b191141692306b
guilhermesvm <guilhermesvmachado@gmail.com>
2025-11-17 21:52:51 -0300

    Corrigido exibição de VFX de explosão ao bater a nave

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -3,16 +3,27 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public GameObject explosionVFX;
+
+    private bool exploded = false;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Asteroid"))
-        {
-            Debug.Log("nave colide");
-            
-            Instantiate(explosionVFX, transform.position, Quaternion.identity);
-            
-            GameManager.Instance.GameOver();
-            Destroy(gameObject);
-        }
+        if (exploded) return;
+        if (!collision.gameObject.CompareTag("Asteroid")) return;
+
+        exploded = true;
+
+        Instantiate(explosionVFX, transform.position, Quaternion.identity);
+
+        Destroy(GetComponent<SpriteRenderer>());
+        Destroy(GetComponent<Collider2D>());
+        Destroy(GetComponent<Rigidbody2D>());
+
+        Invoke(nameof(CallGameOver), 0.6f);
+    }
+
+    private void CallGameOver()
+    {;
+        GameManager.Instance.GameOver();
     }
 }
