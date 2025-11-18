adf43aad4062ac896c085dd6907b97277d686610
guilhermesvm <guilhermesvmachado@gmail.com>
2025-11-17 22:59:43 -0300

    Adicionado efeitos de colisão da nave

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public GameObject explosionVFX;
-
     private bool exploded = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -13,17 +12,21 @@ public class PlayerCollision : MonoBehaviour
 
         exploded = true;
 
+        StartCoroutine(CameraShake.Instance.Shake(0.4f, 0.90f));
+
         Instantiate(explosionVFX, transform.position, Quaternion.identity);
 
         Destroy(GetComponent<SpriteRenderer>());
         Destroy(GetComponent<Collider2D>());
         Destroy(GetComponent<Rigidbody2D>());
 
+        FindObjectOfType<CameraController>().StopFollowing();
+
         Invoke(nameof(CallGameOver), 0.6f);
     }
 
     private void CallGameOver()
-    {;
+    {
         GameManager.Instance.GameOver();
     }
 }
