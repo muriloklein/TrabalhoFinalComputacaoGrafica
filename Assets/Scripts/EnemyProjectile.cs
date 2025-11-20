a18dee730f1845f92f092bc060761bef05747ef8
muriloklein <eu.murilok@gmail.com>
2025-11-20 14:15:41 -0300

    Adicionado Explosão do player quando atingido por projetile inimigo

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 
-public class EnemyBullet : MonoBehaviour
+public class EnemyProjectile : MonoBehaviour
 {
+    public GameObject explosionVFX;
+    public AudioClip hitSound;
     public float speed = 6f;
     public float lifeTime = 4f;
+
     private Rigidbody2D rb;
+    private bool exploded = false;
 
     void Awake()
     {
@@ -18,29 +22,45 @@ public class EnemyBullet : MonoBehaviour
 
     void Update()
     {
-        // Se a velocidade é zero, não rotaciona
         if (rb.linearVelocity.sqrMagnitude > 0.01f)
         {
             float angle = Mathf.Atan2(rb.linearVelocity.y, rb.linearVelocity.x) * Mathf.Rad2Deg;
-
-            // Ajuste dependendo do sprite
             float spriteOffset = -90f;
-
             transform.rotation = Quaternion.Euler(0, 0, angle + spriteOffset);
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !exploded)
         {
-            GameManager.Instance.GameOver();
-            Destroy(gameObject);
+            exploded = true;
+
+            AudioSource.PlayClipAtPoint(hitSound, transform.position);
+
+            Instantiate(explosionVFX, transform.position, Quaternion.identity);
+
+            if (CameraShake.Instance != null)
+                StartCoroutine(CameraShake.Instance.Shake(0.4f, 0.9f));
+
+            if (FindObjectOfType<CameraController>() != null)
+                FindObjectOfType<CameraController>().StopFollowing();
+
+            Destroy(GetComponent<SpriteRenderer>());
+            Destroy(GetComponent<Collider2D>());
+            Destroy(GetComponent<Rigidbody2D>());
+
+            Invoke(nameof(CallGameOver), 0.6f);
         }
 
         if (other.CompareTag("Asteroid"))
         {
             Destroy(gameObject);
         }
+    }
+
+    private void CallGameOver()
+    {
+        GameManager.Instance.GameOver();
     }
 }
